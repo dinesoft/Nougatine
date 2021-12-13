3c81d91130b4f66d1f8024a98d8f028b2178ab2a
alexdav2b <32898585+alexdav2b@users.noreply.github.com>
2021-12-13 13:00:06 +0100

    Add request extract yesterday data

--- extract.py
+++ extract.py
@@ -1,9 +1,34 @@
+import json
+
 import requests
+from datetime import date, timedelta
 
-params = (
-    ('limit', '10'),
-    ('offset', '0'),
-    ('timezone', 'UTC'),
-)
 
-response = requests.get('https://opendata.paris.fr/api/v2/catalog/datasets/comptages-routiers-permanents/records', params=params)
+def daily_request():
+    yesterday = date.today() - timedelta(days=1)
+
+    # Request
+    headers = {
+        'accept': '*/*',
+    }
+
+    params = (
+        # ('limit', "1"),
+        ('where', 't_1h >=  \'{}\''.format(yesterday)),
+        ('offset', '0'),
+        ('timezone', 'UTC'),
+    )
+
+    response = requests.get(
+        'https://opendata.paris.fr/api/v2/catalog/datasets/comptages-routiers-permanents/exports/csv', headers=headers,
+        params=params)
+
+    data = response.content.decode('utf8')
+
+    # Save file
+    with open("data/raw/raw_data.csv", "w") as fo:
+        fo.write(data)
+
+
+if __name__ == '__main__':
+    daily_request()
