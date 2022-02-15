34242c0459dce9a173639e7e142437aff8f105b8
OUAMAR KHALIDA <khalida.ouamar@etu.unistra.fr>
2022-02-15 16:44:32 +0100

    remove comments

--- dag.py
+++ dag.py
@@ -13,9 +13,7 @@ MONTH = '{{ macros.ds_format(ds, "%Y-%m-%d", "%m") }}'
 YESTERDAY = '{{ macros.ds_format(macros.ds_add(ds, 1), "%Y-%m-%d", "%d") }}'
 
 dag = DAG(
-    # TODO : compléter le nom du DAG avec votre nom de gorupe dedans
     dag_id='group3',
-    # compléter le schedule interval
     schedule_interval='* * 0 0 0',
     max_active_runs=1,
     start_date=datetime(2022, 2, 14),
@@ -23,7 +21,6 @@ dag = DAG(
 
 @task(task_id='download_raw_data', dag=dag)
 def download_raw_data(year, month, day):
-    # compléter la valeur
     raw_data_path = "/data/g3/raw"
 
     url = f'https://opendata.paris.fr/api/v2/catalog/datasets/comptages-routiers-permanents/exports/csv?refine=t_1h%3A{year}%2F{month}%2F{day}&timezone=UTC'
@@ -39,7 +36,6 @@ download_raw_data = download_raw_data(YEAR, MONTH, YESTERDAY)
 
 clean_data = BashOperator(
     task_id="spark_job_clean",
-    #  compléter le spark submit
     bash_command=f"spark-submit --master yarn --deploy-mode cluster --jars clean.jar --class Clean.main  /data/g3/raw/data-{YEAR}-{MONTH}-{YESTERDAY}.csv /data/g3/clean/data-{YEAR}-{MONTH}-{YESTERDAY}.parquet",
     dag=dag
 )
@@ -48,7 +44,6 @@ clean_data = BashOperator(
 
 transform_data = BashOperator(
     task_id="spark_job_transform",
-    # TODO : compléter le spark submit
     bash_command=f"spark-submit --master yarn --deploy-mode cluster --jars clean.jar --class Jointure.main /data/g3/clean/data-{YEAR}-{MONTH}-{YESTERDAY}.parquet /data/g3/join/trust.parquet",
     dag=dag
 )
